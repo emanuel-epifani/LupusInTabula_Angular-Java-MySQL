906025cb0fa2354853b5cb40cf6acb49965d74a7
Emanuel Epifani <emanuel.epifani.m@gmail.com>
2022-04-11 20:47:09 +0200

    finito esempio completo di query con prepareStatement x spunto prossime volte

--- 2. BE-Java/src/main/java/com/example/be_java/esempioConnDB/CRUDrepository.java
+++ 2. BE-Java/src/main/java/com/example/be_java/esempioConnDB/CRUDrepository.java
@@ -39,39 +39,44 @@ public class CRUDrepository {
         }
 
         //READ
-//
-//        public static void getInfoDB() {
-//
-//            try{
-//                //apro una connessione --> DriverManager.getConnection()
-//                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-//                //definisco la QUERY
-//                String QUERY = "SELECt nome FROM Studenti WHERE age = ? AND  scuola = ?";
-//
-//                //creo un oggetto di tipo statement in cui immagazzinare la mia QUERY
-//                PreparedStatement pstmt = conn.prepareStatement(QUERY);
-//                int age;
-//                pstmt.setInt(27, age);
-//                String scuola;
-//                pstmt.setString("Engim", scuola);
-//
-//                //eseguo la QUERY
-//                ResultSet rs = pstmt.executeQuery(QUERY);
-//
-//                // Extract data from result set
-//                while (rs.next()) {
-//
-//                    System.out.println(rs);//x debug -->x veder cos ho estratto
-//                    // faccio cose con i dati ottenuto
-//                    System.out.print("ID: " + rs.getInt("id"));
-//                    System.out.print(", Age: " + rs.getInt("age"));
-//                    System.out.print(", First: " + rs.getString("first"));
-//                    System.out.println(", Last: " + rs.getString("last"));
-//                }
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
+
+
+        public static void getInfoDB() {
+
+            try{
+                //1. apro una connessione col db--> DriverManager.getConnection()
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+                //2. definisco la QUERY, con preparedStatement, quindi senza concatenazione stringhe, ma con segnaposto ? al posto delle variabili
+                String QUERY = "SELECT nome FROM Studenti WHERE age = ? AND  scuola = ?";
+
+                //3. creo un oggetto di tipo prepareStatement in cui inserisco la mia QUERY, e definisco i valori da inserire al posto dei ?
+                PreparedStatement pstmt = conn.prepareStatement(QUERY);
+                pstmt.setInt(1, 27);
+                pstmt.setString(2, "Engim");
+
+                //4. eseguo la QUERY e immagazzino il valore nell'oggetto di tipo ResultSet
+                    //se query di R (SELECT) - pstmt.executeQuery();
+                    //se query CUD (CREATE,UPDATE;DELETE) - pstmt.executeUpdate();
+                ResultSet rs = pstmt.executeQuery(QUERY);
+
+                //5.  Estraggo i dati immagazzinati nel ResultSet e ci faccio qcosa mi serve
+                while (rs.next()) { // .next() fa avanzare alle righe successive, quando righe finite ritorna false
+
+                    // faccio cose con i dati ottenuto (si accede agi attributi con il metodo "getTIPO()" ) getInt, getString, getBoolean ecc
+                    System.out.print("ID: " + rs.getInt("id"));
+                    System.out.print(", Age: " + rs.getInt("age"));
+                    System.out.print(", First: " + rs.getString("first"));
+                    System.out.println(", Last: " + rs.getString("last"));
+
+                    pstmt.close(); //chiudo lo statement
+                    conn.close(); //chiudo la connessione
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
 
         //UPDATE
         public static void updateInfoDB() {
